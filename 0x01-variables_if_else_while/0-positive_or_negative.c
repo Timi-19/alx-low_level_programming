09e50cb5aa6d36b7f2c779cbebe43fa49129e055
Timi-19 <oluwatosin.e.dowo@gmail.com>
2022-09-09 07:33:35 -0700

    n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -19,3 +20,5 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
+} 
+
