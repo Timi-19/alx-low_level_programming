4075274dc9bd3b372f58fa9a60ae400ea07416be
Timi-19 <104963678+Timi-19@users.noreply.github.com>
2022-09-13 12:35:40 +0100

    update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs - a function that computes the absolute value of an integer
+ * a function that computes the absolute value of an integer
  * @ab: integer input
  * Return: absolute value of ab
  */
