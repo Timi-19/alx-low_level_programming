484bb3cc696fd1aa37f39331cf67cd4a3b5bb185
Timi-19 <104963678+Timi-19@users.noreply.github.com>
2022-09-13 11:39:43 +0100

    002

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,20 @@
 
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase 10 times
- *
- * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
 {
-      int count = 0;
-      char n;
-  
-      while (count < 10)
-      {
-          for (n = 'a'; n <= 'z'; n++)
-          {
-            _putchar(n);
-          }
-          count++;
-          _putchar('\n');
-  }
+	int count = 0; 
+	char n;
+
+	while (count < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		count++;
+		_putchar('\n');
+	}
 }
